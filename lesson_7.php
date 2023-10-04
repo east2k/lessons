df0e65fb1a94285d6f36cbc67067b4063b9c9b26
Christian Estoque <christian.estoque.dev@gmail.com>
2023-10-04 18:47:50 +0800

    Update and Add: updated lesson 6 and 7, added lesson 8 and 9

--- lesson_7.php
+++ lesson_7.php
@@ -5,16 +5,59 @@
     <br />
     <h1>Lesson 7</h1>
     <?php
-    echo "<h3>Variable Types</h3>";
+    echo "<h3>Variable Naming</h3>";
     // Running on command prompt
-    echo " Integers: are whole numbers, without a decimal point, like 4195.
-    <br/> Doubles: are floating-point numbers, like 3.14159 or 49.1.
-    <br/> Booleans: have only two possible values either true or false.
-    <br/> NULL: is a special type that only has one value: NULL.
-    <br/> Strings: are sequences of characters, like 'PHP supports string operations.'
-    <br/> Arrays: are named and indexed collections of other values.
-    <br/> Objects: are instances of programmer-defined classes, which can package up both 
-    <br/> other kinds of values and functions that are specific to the class.";
+    echo 'Variable names must begin with a letter or underscore character.
+    <br/> A variable name can consist of numbers, letters, underscores but you cannot use characters like + , - , % , ( , ) . & , etc';
+    echo "<h3>Variable Scope</h3>";
+
+    echo "<h4>Local Variables</h4>";
+    echo '$x = 4;
+    <br/>function assignx () {
+    <br/>33
+    <br/>$x = 0;
+    <br/>print "\$x inside function is $x. 
+    <br/>";
+    <br/>}
+    <br/>assignx();
+    <br/>print "\$x outside of function is $x. 
+    <br/>";
+    ';
+
+    echo "<h4>Function Parameters</h4>";
+    echo '// multiply a value by 10 and return it to the caller
+    <br/>function multiply ($value) {
+    <br/>$value = $value * 10;
+    <br/>return $value;
+    <br/>}
+    <br/>$retval = multiply (10);
+    <br/>Print "Return value is $retval\n";
+    ';
+
+    echo "<h4>Global Variables</h4>";
+    echo '$somevar = 15;
+    <br/>function addit() {
+    <br/>GLOBAL $somevar;
+    <br/>$somevar++;
+    <br/>print "Somevar is $somevar";
+    <br/>}
+    <br/>addit();
+    ';
+
+    echo "<h4>Static Variables</h4>";
+    echo 'function keep_track() {
+    <br/>STATIC $count = 0;
+    <br/>$count++;
+    <br/>print $count;
+    <br/>print "
+    <br/>";
+    <br/>PHP
+    <br/>35
+    <br/>}
+    <br/>keep_track();
+    <br/>keep_track();
+    <br/>keep_track();
+    ';
     ?>
 </body>
 
